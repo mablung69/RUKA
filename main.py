ca5b7ab68b4bd4485302036762d55dae37e8cbb8
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-13 17:38:02 -0300

    Add closest device seeking

--- main.py
+++ main.py
@@ -5,14 +5,32 @@ import threading
 gpio = GPIOController()
 
 
-def discover_nearby_devices():
-    while True:
-        gpio.block_until_press()
-        print('Looking for devices...')
-        nearby_devices = bluetooth_controller.get_nearby_devices()
-        print('Found these devices: {}'.format(nearby_devices))
+class Controller:
+    def __init__(self):
+        self.closest_device = None
+
+        discoverer_thread = threading.Thread(name='Discoverer',
+                                             target=self.seek_closest_device)
+        discoverer_thread.start()
+
+    def seek_closest_device(self):
+        while True:
+            gpio.block_until_press()
+            print('Looking for devices...')
+            nearby_devices = bluetooth_controller.get_nearby_devices(True)
+
+            if nearby_devices:
+                print('Found these devices: {}'.format(nearby_devices))
+                distances = []
+                for device in nearby_devices:
+                    distances.append(bluetooth_controller.get_rssi(device[0]))
+                print('Distances: {}'.format(distances))
+
+                self.closest_device = nearby_devices[nearby_devices.index(
+                    max(distances))]
+                print('The closest device is: {}'.format(self.closest_device))
+            else:
+                print('Found no devices nearby.')
 
 if __name__ == '__main__':
-    discoverer_thread = threading.Thread(name='Discoverer',
-                                         target=discover_nearby_devices)
-    discoverer_thread.start()
+    controller = Controller()
