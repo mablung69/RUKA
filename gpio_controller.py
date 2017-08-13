9364809a854522009c92f8ff13a02c41b532aebc
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-13 17:47:26 -0300

    Add cleanup method and initialization checking

--- gpio_controller.py
+++ gpio_controller.py
@@ -3,14 +3,25 @@ import RPi.GPIO as GPIO
 
 class GPIOController:
     button_pin = 17
+    initialized = False
 
     def __init__(self):
+        if GPIOController.initialized:
+            print('Warning: GPIO already initialized.')
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
         GPIO.setup(GPIOController.button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIOController.initialized = True
+
+    @staticmethod
+    def cleanup():
+        print('Cleaning GPIO pins.')
+        GPIO.cleanup()
 
     @staticmethod
     def block_until_press():
+        if not GPIOController.initialized:
+            raise RuntimeError('GPIO not initialized.')
         while True:
             state = GPIO.input(GPIOController.button_pin)
             if not state:
