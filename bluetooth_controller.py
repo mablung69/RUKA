841af584510f05f1b36e7a708d7d8cbb51de721f
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-13 15:35:20 -0300

    Rename and move file, and create bluetooth_controller.py file

---
+++ bluetooth_controller.py
@@ -0,0 +1,25 @@
+import bluetooth
+import execnet
+
+
+def get_nearby_devices(names=False):
+    return bluetooth.discover_devices(names)
+
+
+def get_rssi(address):
+    version = '2.7'
+    module_ = 'bluetooth_proximity.bt_rssi'
+    function_ = 'get_rssi'
+
+    gateway = execnet.makegateway('popen//python=python{}'.format(version))
+    channel = gateway.remote_exec('''
+        from {} import {} as the_function
+        channel.send(the_function(*channel.receive()))
+    '''.format(module_, function_))
+    channel.send([address])
+    return channel.receive()
+
+
+if __name__ == '__main__':
+    address = '7C:01:91:4D:FA:16'
+    print(get_rssi(address))
