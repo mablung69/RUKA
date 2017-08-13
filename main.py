564efec5fcbb2b198195c896fa625bf6afcbaec3
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-08-13 17:21:30 -0300

    Create main file and discoverer function

---
+++ main.py
@@ -0,0 +1,19 @@
+from gpio_controller import GPIOController
+import bluetooth_controller
+import threading
+
+gpio = GPIOController()
+
+
+def discover_nearby_devices():
+    while True:
+        gpio.block_until_press()
+        print('Looking for devices...')
+        nearby_devices = bluetooth_controller.get_nearby_devices()
+        print('Found these devices: {}'.format(nearby_devices))
+
+if __name__ == '__main__':
+    discoverer_thread = threading.Thread(name='Discoverer',
+                                         target=discover_nearby_devices,
+                                         daemon=True)
+    discoverer_thread.start()
